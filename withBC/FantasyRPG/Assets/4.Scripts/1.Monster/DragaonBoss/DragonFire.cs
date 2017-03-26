f1ac56ce970dbcbbbf8bec292a1a46e0349b6e78
asdf <jbc04719@naver.com>
2017-03-27 08:33:47 +0900

    수정

---
+++ withBC/FantasyRPG/Assets/4.Scripts/1.Monster/DragaonBoss/DragonFire.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonFire : MonoBehaviour {
+
+    public GameObject DragonFlame;
+    private DragonAnimation D_Animation;
+
+
+	// Use this for initialization
+	void Start () {
+        D_Animation = GetComponentInParent<DragonAnimation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        switch(D_Animation.NowState)
+        {
+            case DragonAnimation.D_STATE.D_FIRE:
+                if(D_Animation.OnceAttackCheck)
+                {
+                    //Instantiate(DragonFlame);
+                    StartCoroutine(EffectManager.Instance.CreatEffect("DragonFire",
+                          this.gameObject.transform.position, this.transform.rotation, 0.0f, 0.25f));
+
+                    //StartCoroutine(EffectManager.Instance.DestroyEffect(collider.gameObject, 0.0f));
+
+                    D_Animation.OnceAttackCheck = false;
+                }
+                break;
+            case DragonAnimation.D_STATE.D_FLY_FIRE:
+                if (D_Animation.OnceAttackCheck)
+                {
+                    StartCoroutine(EffectManager.Instance.CreatEffect("DragonFire",
+                          this.gameObject.transform.position, this.GetComponentInParent<DragonMove>().gameObject.transform.rotation, 0.0f, 3.5f));
+
+                    //Instantiate(DragonFlame);
+                    D_Animation.OnceAttackCheck = false;
+                }
+                break;
+        }
+	}
+}
